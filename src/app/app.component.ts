086df63f18d865cef398dfb3e6921f317ffff86a
esputnix <pasha.ivanoff@icloud.com>
2020-08-18 14:47:48 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 ﻿import { Component } from '@angular/core';
 
-import { HttpService } from '../http.service';
+import { HttpService } from './http.service';
 
 @Component({
     selector: 'app-root',

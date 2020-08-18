223a0b4fa27d820cd058dce74c3039158c20b7ca
esputnix <pasha.ivanoff@icloud.com>
2020-08-18 14:45:26 -0700

    1 file modified

--- src/http.service.ts
+++ src/http.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { timer } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class HttpService {
+  jsonFile = '../assets/products.json';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-
+  getProductsOverHTTP(): Observable<any> {
+    return this.http.get(this.jsonFile);
+  }
   getData(paginginformation) {
     return http.get("../assets/user.json")
   }
-
-
 }

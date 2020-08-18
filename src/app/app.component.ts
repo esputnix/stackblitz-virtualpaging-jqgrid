71de1563f6730bdae872ee7c7a087420346a1583
esputnix <pasha.ivanoff@icloud.com>
2020-08-18 15:07:03 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -75,8 +75,8 @@ export class AppComponent {
           '2.25', '1.5', '3.0', '3.3', '4.5', '3.6', '3.8', '2.5', '5.0', '1.75', '3.25', '4.0'
       ];
 
-    makeHttpRequest(params) {
-      this.http.getData(params);
+    makeHttpRequest() {
+      this.http.getData("params");
     }
 
     generateData(startindex: number, endindex: number): any {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { jqxGridComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxgrid.ts';
+import { jqxButtonComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxbuttons.ts';
 
 @NgModule({
-    declarations: [AppComponent, jqxGridComponent],
+    declarations: [AppComponent, jqxGridComponent, jqxButtonComponent],
     imports: [BrowserModule, FormsModule, HttpClientModule],
     providers: [],
     bootstrap: [AppComponent]
